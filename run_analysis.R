e2048cd916622012f748cbfe3219c607e56357aa
filip-cz <filip-cz@users.noreply.github.com>
2014-09-20 11:02:30 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 #read test files
-activity_labels <- read.table("./Data/activity_labels.txt")
-features <- read.table("./Data/features.txt")
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 
-subject_test <- read.table("./Data/test/subject_test.txt")
-X_test <- read.table("./Data/test/X_test.txt")
-y_test <- read.table("./Data/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
-subject_train <- read.table("./Data/train/subject_train.txt")
-X_train <- read.table("./Data/train/X_train.txt")
-y_train <- read.table("./Data/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
 #merge data
 subject <- rbind(subject_test, subject_train)
